a1ec76575ac7cd9b7239a3ab46d1a5ebd242a2fc
Mateus Berlitz <wobetoberlitz@gmail.com>
2022-12-05 15:49:41 -0300

    migrated to new api endpoint

--- next.config.js
+++ next.config.js
@@ -10,9 +10,11 @@ const nextConfig = {
     return config;
   },
   env: {
-    PARTNER_PUBLIC_API: 'https://lance-partner-backend.herokuapp.com/public/api/',
+    PARTNER_PUBLIC_API: 'https://partner-api.lanceconsorcio.com.br/public/api/',
+    PARTNER_PUBLIC_LOCAL_API: 'https://partner-api.lanceconsorcio.com.br/public/api/',
+   // PARTNER_PUBLIC_API: 'https://lance-partner-backend.herokuapp.com/public/api/',
     PARTNER_PUBLIC_STORAGE: '', //https://lance-partner.s3.sa-east-1.amazonaws.com/
-    PARTNER_PUBLIC_LOCAL_API: 'https://lance-partner-backend.herokuapp.com/public/api/',
+    //PARTNER_PUBLIC_LOCAL_API: 'https://lance-partner-backend.herokuapp.com/public/api/',
     PARTNER_PUBLIC_LOCAL_STORAGE: '',//'http://localhost/Novidades/lance-partner-backend/public/storage/',
   },
 }
